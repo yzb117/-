9ba63fcb7efaf96178ee0e0e0e67a4d3e80373e2
yzb117 <2865789691@qq.com>
2026-02-21 19:54:25 +0800

    网易云音乐

--- 网易云.js
+++ 网易云.js
@@ -214,9 +214,11 @@ for(let i= 0;i<10;i++){
     div.classList.add('wrapper');
     body.append(div);
 }
-const wrapper= document.querySelectorAll('.wrapper')
+
 allList.forEach(item=>{
+    
     item.addEventListener('click',()=>{
+    const wrapper= document.querySelectorAll('.wrapper')
     let n = item.dataset.id;
     wrapper.forEach(item=>{
         item.classList.remove('active');
@@ -252,75 +254,193 @@ wrapper2_nav2.forEach(item=>{
 
 
 
+// 歌单
+const songsList = document.querySelectorAll('.wrapper2_slider .single,.wrapper2_slider2 .single')
+for(let i= 0;i<13;i++){
+    const div = document.createElement('div');
+    div.classList.add('wrapper');
+    body.append(div);
+}
+songsList.forEach(item=>{
+    
+    item.addEventListener('click',()=>{
+    const wrapper= document.querySelectorAll('.wrapper')
+    let n = item.dataset.id;
+    wrapper.forEach(item=>{
+        item.classList.remove('active');
+    })
+    wrapper[n].classList.add('active');
+    })
+})
 
+const songLists=[
+{
+    songs:['江南','不流泪的机场','交换余生','关键词','飞云之下','她说','可惜没如果','裹着心的光','醉赤壁','愿与愁','不为谁而作的歌','那些你很冒险的梦']
+},
+{
+    songs:["花海", "晴天", "七里香", "告白气球", "一路向北", "小幸运", "后来", "遇见", "勇气", "演员"]
+},
+{
+    songs:["In The Shadow Of The Sun","Shape of You", "Uptown Funk", "Perfect", "Someone Like You", "Hello", "Dynamite", "Blinding Lights", "Levitating", "Watermelon Sugar", "Dance Monkey"]
+},
+{
+    songs:["River Flows in You", "Kiss the Rain", "Luv Letter", "The Truth That You Leave", "Merry Christmas Mr. Lawrence", "Clair de Lune", "Canon in D", "Air on the G String", "Moon River", "La Vie En Rose"]
+},
+{
+    songs:["泡沫", "光年之外", "来自天堂的魔鬼", "句号", "多远都要在一起", "喜欢你", "倒数", "A.I.N.Y.", "我的秘密", "画"]
+},
+{
+    songs:["消愁", "像我这样的人", "一荤一素", "平凡的一天", "借", "不染", "无问", "水乡", "一程山路", "东北民谣"]
+},
+{
+    songs:["遇见", "天黑黑", "开始懂了", "绿光", "遇见", "我怀念的", "雨天", "逆光", "风筝", "神奇"]
+},
+{
+    songs:["晴天", "七里香", "花海", "告白气球", "一路向北", "夜曲", "青花瓷", "稻香", "以父之名", "珊瑚海"]
+},
+{
+    songs:["倔强", "温柔", "知足", "突然好想你", "恋爱ing", "私奔到月球", "天使", "我不愿让你一个人", "如烟", "干杯"]
+},
+{
+    songs:["十年", "浮夸", "富士山下", "K歌之王", "好久不见", "爱情转移", "红玫瑰", "白玫瑰", "孤独患者", "你的背包"]
+},
+{
+    songs:["红豆", "传奇", "暧昧", "人间", "匆匆那年", "致青春", "因为爱情", "流年", "笑忘书", "梦中人"]
+},
+{
+    songs:["模特", "李白", "年少有为", "戒烟", "老街", "麻雀", "爸爸妈妈", "耳朵", "不将就", "满座"]
+}
+]
+function createSongList(item,n){
+    const songList_img=document.createElement('img')
+    const songList_p=document.createElement('p');
+    const songList_ul= document.createElement('ul');
+    songList_img.classList.add('songList_wrapperImg')
+    songList_p.classList.add('songList_wrapperP');
+    songList_ul.classList.add('songList_wrapperUl');
+    const container=document.querySelector(`.single[data-id="${n}"]`)
+    songList_img.src=container.querySelector('img').src;
+    songList_p.innerHTML=container.querySelector('.footer').innerHTML
+    songLists[n-13].songs.forEach((song,index)=>{
+        const li = document.createElement('li')
+       
+        li.innerHTML=index+1
+        li.innerHTML+=`<p class="song">${song}</p>`;
+        songList_ul.append(li)
+    })
+    item.append(songList_img)
+    item.append(songList_p)
+    item.append(songList_ul)
+}
+const wrapper = document.querySelectorAll('.wrapper')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for(let n=13;n<25;n++)
+{
+    createSongList(wrapper[n],n)
+}
 
 // 歌曲
-function getSong(keywords){
-    return fetch(`http://localhost:3000/search?keywords=${keywords}`)
-    .then(res=>res.json())
-    .then(res=>localStorage.setItem(`${keywords}`,JSON.stringify(res)))
-    .catch(err=>{
-    console.error(err);
-    });
-}
-const song='海阔天空'
-getSong(song)
-.then(()=>{
-console.log(localStorage.getItem('海阔天空'))
-})
-async function playSong(songId) {
-  try {
-    const res = await fetch(`http://localhost:3000/song/url?id=${songId}`);
+const audio = new Audio()
+async function getSong(keywords) {
+    try{ const res = await fetch(`http://localhost:3000/search?keywords=${keywords}`)
     const data = await res.json();
-
-    // 检查 data 数组是否存在且不为空
-    if (data.data && data.data.length > 0) {
-      const playUrl = data.data[0].url;
-      if (playUrl) {
-        const audio = new Audio(playUrl);
+    const song = data.result.songs[0]
+    const songId = song.id
+    const res2 = await fetch(`http://localhost:3000/song/url?id=${songId}`)
+    const data2 = await res2.json();
+    if(data2.data&&data2.data.length>0)
+    {
+        const songUrl = data2.data[0].url
+        if(songUrl)
+        {
+            audio.src=songUrl
+            audio.play()
+        }else{
+            console.log('失败');
+            
+        }
+    }
+    }catch(err){
+        console.log('error',err);
         
+    }
+}
 
-        audio.play();
-        console.log('正在播放:', playUrl);
-      } else {
-        console.log('无播放链接');
-      }
-    } 
-  } catch (err) {
-    console.error('播放失败:', err);
-  }
-}
-const text = document.querySelector('.slider .slider_container:nth-child(1)')
-text.addEventListener('click',()=>{
-    playSong(1357375695)
-})
-async function getPlayUrl(songId) {
-  try {
-    const res = await fetch(`http://localhost:3000/song/url?id=${songId}`);
+
+async function getSongInfo(keywords) {
+    try{
+    const res = await fetch(`http://localhost:3000/search?keywords=${keywords}`) 
+    
     const data = await res.json();
-    console.log('播放链接:', data);
-    return data;
-  } catch (err) {
-    console.error('获取播放链接失败:', err);
-  }
+    // console.log('数据3',data);
+    
+    const singer =await data.result.songs[0].artists.map(artist=>artist.name).join('/')
+    // console.log('歌手',singer)
+    const song = data.result.songs[0]
+    const songId = song.id;
+    const infoRes = await fetch(`http://localhost:3000/song/detail?ids=${songId}`)
+    const infoData = await infoRes.json();
+    // console.log('数据',infoData);
+    const singerpic = infoData.songs[0].al.picUrl
+    // const songInfo =infoData.songs[0];
+    // const singer= songInfo.ar.map(singer=>singer.name).join('/');
+    return {
+        picurl:singerpic,
+        singer:singer
+    }
+    }catch{}
 }
+// 播放&&底部歌单
+const singerimg = document.querySelector('.playList img')
+const song_Name = document.querySelector('.playList .songinfo p:first-child')
+const singerName = document.querySelector('.playList .songinfo p:nth-child(2)')
+const songs = document.querySelectorAll('.song')
+songs.forEach(item=>{
+    item.addEventListener('click',async (e)=>{
+        e.preventDefault();
+        
+        const songName = item.textContent.trim();
+        try{
+            const song_info = await getSongInfo(songName)
+            singerimg.src= song_info.picurl
+            song_Name.innerHTML=songName
+            singerName.innerHTML=song_info.singer
+            audio.pause()
+            
+            audio.currentTime=0
+            await getSong(songName)
+        } catch (err){
+            console.log('error',err);
+            
+        }
+    })
+})
+// 暂停
+const songstop = document.querySelector('#stop')
+songstop.addEventListener('click',(e)=>{
+    e.preventDefault();
+    if(singerName.innerHTML!='')
+    {
+        if(songstop.className==='iconfont icon-stop')
+        {
+            songstop.className='iconfont icon-bofang1'
+            audio.pause()
+        }else{
+            songstop.className='iconfont icon-stop'
+            audio.play()
+        }
+    }
+})
+
+
+
+
+
+
+
+
+
+
 
 
 
